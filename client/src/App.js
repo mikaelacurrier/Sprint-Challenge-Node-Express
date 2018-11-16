e91f60fe4a687bdd6e4307370b97ae9069483c60
mikaelacurrier <mikaela.currier@gmail.com>
2018-11-16 12:42:04 -0500

    added simple front-end, displays projects

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: [],
+      actions: []
+    }
+  }
+  componentDidMount = () => {
+    axios.get('http://localhost:9000/api/projects')
+    .then(response => {
+      this.setState({ projects: response.data })
+    })
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <div className="projects">
+         Projects:
+         {this.state.projects.map((project, key) => {
+           return (
+             <div key={key}>
+             <h4>{project.name}</h4>
+             <p>{project.description}</p>
+             </div>
+           )
+         })}
+       </div>
       </div>
     );
   }
